24d51180fb285b150980394ee9df9c867203cff2
David A <mercenary_88@hotmail.com>
2018-11-08 13:32:31 +0100

    Mitt namn som titel och lagt till unittest

---
+++ DaBank/UnitTesting/DaBankUnitTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class DaBankUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
