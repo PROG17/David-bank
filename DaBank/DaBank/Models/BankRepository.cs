46079dbc416527919e3a3579ffb1b986d358d59c
David A <mercenary_88@hotmail.com>
2018-11-08 14:22:18 +0100

    lagt till models och metod för skapa kunder och konton samt skrivit ut lista för kunder och konton på startsidan

---
+++ DaBank/DaBank/Models/BankRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DaBank.Models
+{
+    public class BankRepository
+    {
+        public List<Customer> Customers { get; set; }
+    }
+}
---
+++ DaBank/DaBank/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DaBank.Models
+{
+    public class Customer
+    {
+       
+        public string Name { get; set; }
+        public List<Account> Account;
+        
+        public Customer(string name, List<Account> account)
+        {
+            Name = name;
+            Account = account;
+        }
+
+    }
+}
