c670af075d08f892b1238f5e93bf4b2ce4916f1c
David A <mercenary_88@hotmail.com>
2018-11-10 13:40:38 +0100

    work with transactions in depowith controller

--- DaBank/DaBank/Controllers/DepoWithController.cs
+++ DaBank/DaBank/Controllers/DepoWithController.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DaBank.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DaBank.Controllers
 {
     public class DepoWithController : Controller
     {
-        public IActionResult DepoWith()
+        public IActionResult DepoWith(string error)
         {
+            if(error != null)
+            {
+                ViewBag.Error = error;
+            }
             return View();
         }
+
+        public IActionResult Transaction(string submitButton, decimal value, int account)
+        {
+            var bankController = new BankController();
+
+            var requestedAccount = bankController.CreateBankRep().Customers.SelectMany(x=> x.Account.Where(y=> y.AccountNumber == account)).FirstOrDefault();
+
+            if (requestedAccount != null)
+            {
+                switch (submitButton)
+                {
+                    case "Deposit":                        
+                        return (View(bankController.Deposit(requestedAccount, value)));
+                    case "Withdraw":
+                        var result = bankController.Withdraw(requestedAccount, value);
+                        if(result != null)
+                        {
+                            return (View(result));
+                        }
+                        return RedirectToAction("DepoWith", new { error = "Not enough balance to withdraw." });
+                    default:
+
+                        return RedirectToAction("DepoWith");
+                }
+            }
+
+            return RedirectToAction("DepoWith");
+
+        }
+
     }
 }
