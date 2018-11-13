db223180e3f2ea718e88e991a20a4d68b6e2bbf0
username <test@example.com>
2018-11-13 11:45:32 +0100

    stöd för överföring

---
+++ DaBank/DaBank/Models/Transaction.cs
@@ -0,0 +1,9 @@
+﻿namespace DaBank.Models
+{
+    public class Transaction
+    {
+        public int AccountFrom { get; set; }
+        public int AccountTo { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
